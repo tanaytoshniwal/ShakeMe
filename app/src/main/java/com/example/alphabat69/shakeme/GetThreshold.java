e76f421ce3ebe73597fb8e23d49a6c84817cc494
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-20 18:15:17 +0530

    getting threshold

---
+++ app/src/main/java/com/example/alphabat69/shakeme/GetThreshold.java
@@ -0,0 +1,14 @@
+package com.example.alphabat69.shakeme;
+
+/**
+ * Created by AlphaBAT69 on 20-11-2017.
+ */
+
+public class GetThreshold {
+    static float get(int i){
+        final float MAX= 9.0f;
+        float f=0.90f;
+
+        return MAX-f*i;
+    }
+}
